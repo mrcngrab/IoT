7e99965a0ecfed60ee0cef5e8f91da50cf99a159
Marcin <mrcngrab@gmail.com>
2016-06-17 18:34:52 +0200

    removed

--- MyApplication/app/src/main/java/com/mrcn/homecontroller/viewmodel/BaseViewModel.java
+++
@@ -1,9 +0,0 @@
-package com.mrcn.homecontroller.viewmodel;
-
-import android.databinding.BaseObservable;
-
-/**
- * Created by gmra on 2016-05-11.
- */
-public class BaseViewModel extends BaseObservable {
-}
--- MyApplication/app/src/main/java/com/mrcn/homecontroller/viewmodel/fragment/MainMenuViewModel.java
+++
@@ -1,9 +0,0 @@
-package com.mrcn.homecontroller.viewmodel.fragment;
-
-import com.mrcn.homecontroller.viewmodel.BaseViewModel;
-
-/**
- * Created by gmra on 2016-05-11.
- */
-public class MainMenuViewModel extends BaseViewModel {
-}
